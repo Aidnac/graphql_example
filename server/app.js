169386cbc81b71902a175c3926d5655489931028
Aidnac <calex6658@gmail.com>
2022-08-21 14:29:16 +0300

    Installed Mongo DB and connected it online in mlab.com

--- server/app.js
+++ server/app.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
 
+//connect to mlab database
+mongoose.connect(
+  'mongodb+srv://aidnac:3ri9TiKxxYYev7n7@cluster0.1dpa1lf.mongodb.net/?retryWrites=true&w=majority'
+);
+mongoose.connection.once('open', () => {
+  console.log('connected to database');
+});
 //middleware tells graphqlHTTP
 //to handle everthing from /graghql
 app.use(
