def14fc306d1ba517005bc085b51cec71f3d0a58
Aidnac <calex6658@gmail.com>
2022-08-21 13:05:27 +0300

    Added the schema and tested it in graphi gui

--- server/app.js
+++ server/app.js
@@ -1,6 +1,22 @@
 const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema/schema');
 
 const app = express();
+
+//middleware tells graphqlHTTP
+//to handle everthing from /graghql
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    /**This says we want to use the garphiQL tool when
+     * we go to this address in the browser note the
+     * spelling of 'graphi ql' not 'graphical'
+     */
+    graphiql: true,
+  })
+);
 
 app.listen(4000, () => {
   console.log('now listening for requests on port 4000');
---
+++ server/schema/schema.js
@@ -0,0 +1,73 @@
+const graghql = require('graphql');
+const _ = require('lodash');
+
+/** We are destructuring ie getting GraphQLObjectType
+ * obketc from graghql*/
+const {
+  GraphQLObjectType,
+  GraphQLSchema,
+  GraphQLString,
+  GraphQLID,
+  GraphQLInt,
+} = graghql;
+
+//dummy data array
+var books = [
+  { name: 'Name of the Wind', genre: 'Fantansy', id: '1' },
+  { name: 'The Final Empire', genre: 'Fantansy', id: '2' },
+  { name: 'The Long Earth', genre: 'Sci-Fi', id: '3' },
+];
+
+var authors = [
+  { name: 'Patrick Rothfuss', age: 44, id: '1' },
+  { name: 'Brandon Sanderson', age: 42, id: '2' },
+  { name: 'Terry Pratchett', age: 66, id: '3' },
+];
+
+const BookType = new GraphQLObjectType({
+  name: 'Book',
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    genre: { type: GraphQLString },
+  }),
+});
+
+const AuthorType = new GraphQLObjectType({
+  name: 'Author',
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    age: { type: GraphQLInt },
+  }),
+});
+
+//RootQuerys are where to initially jump into the graph
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    book: {
+      type: BookType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        //code to get data from the db /other sources
+
+        //We could use vanilala javascript but lodash
+        //will simplify our life
+        return _.find(books, { id: args.id });
+      },
+    },
+    author: {
+      type: AuthorType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return _.find(authors, { id: args.id });
+      },
+    },
+  },
+});
+
+//this is what they will be querrying in the frontend
+module.exports = new GraphQLSchema({
+  query: RootQuery,
+});
