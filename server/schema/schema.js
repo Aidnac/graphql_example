6be1fa87414cbeac0db42955a874653462ca2195
Aidnac <calex6658@gmail.com>
2022-08-21 13:31:12 +0300

    GraphQLList

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -9,6 +9,7 @@ const {
   GraphQLString,
   GraphQLID,
   GraphQLInt,
+  GraphQLList,
 } = graghql;
 
 //dummy data array
@@ -16,6 +17,9 @@ var books = [
   { name: 'Name of the Wind', genre: 'Fantansy', id: '1', authorId: '1' },
   { name: 'The Final Empire', genre: 'Fantansy', id: '2', authorId: '2' },
   { name: 'The Long Earth', genre: 'Sci-Fi', id: '3', authorId: '3' },
+  { name: 'The Hero of Ages', genre: 'Fantansy', id: '4', authorId: '2' },
+  { name: 'The colour of Magic', genre: 'Fantansy', id: '5', authorId: '3' },
+  { name: 'The Light Fantastic', genre: 'Fantansy', id: '6', authorId: '3' },
 ];
 
 var authors = [
@@ -45,6 +49,12 @@ const AuthorType = new GraphQLObjectType({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
     age: { type: GraphQLInt },
+    books: {
+      type: new GraphQLList(BookType),
+      resolve(parent, args) {
+        return _.filter(books, { authorId: parent.id });
+      },
+    },
   }),
 });
 
